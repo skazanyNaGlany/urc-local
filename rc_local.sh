1f7d3f1a73a0656d5a27533c10ab9595b685b393
Paweł <screamingbox@gmail.com>
2026-01-21 22:38:44 +0100

    rc_local.sh update

--- rc_local.sh
+++ rc_local.sh
@@ -3,29 +3,19 @@
 # Sample RC file which you can use as your starting point
 # It will run apps over and over again, but you can pass your own code here
 
-_USER=${USER}
-
-# get name of the real user
-if [ -n "$SUDO_USER" ]; then
-	_USER=${SUDO_USER}
+# run Double Commander if not running
+if [ -z "$(pidof doublecmd)" ]; then
+    doublecmd > /dev/null 2>&1 &
 fi
 
-while true; do
-    # run Double Commander if not running
-    if [ -z "$(pidof doublecmd)" ]; then
-        nohup sudo -u ${_USER} doublecmd > /dev/null 2>&1 &
-    fi
+# run Sublime Text if not running
+if [ -z "$(pidof subl)" ]; then
+    subl > /dev/null 2>&1 &
+fi
 
-    # run Kate if not running
-    if [ -z "$(pidof kate)" ]; then
-        nohup sudo -u ${_USER} kate > /dev/null 2>&1 &
-    fi
+# run Visual Studio Code if not running
+if [ -z "$(pidof code)" ]; then
+    code > /dev/null 2>&1 &
+fi
 
-    # run Visual Studio Code if not running
-    if [ -z "$(pidof code)" ]; then
-        nohup sudo -u ${_USER} code > /dev/null 2>&1 &
-    fi
-
-    sleep 3
-done
 
